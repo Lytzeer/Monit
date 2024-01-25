c002bc0039bf3420d27cbc5ae23bfc705439f4e2
Lytzeer <lukasportier@gmail.com>
2024-01-25 11:41:08 +0100

    bash script

--- init.sh
+++ init.sh
@@ -9,7 +9,6 @@ groupadd monit
 usermod -aG monit monit
 pip install -r ./requirement.txt
 mkdir -p /etc/monit/conf.d /var/monit /var/log/monit
-chown -R monit:monit /etc/monit/conf.d /var/monit /var/log/monit
 cp ./conf/conf.json ./conf/api_conf.json /etc/monit/conf.d/
 cp ./app/api.py ./app/monit.py /home/monit/
-chown -R monit:monit /home/monit
+chown -R monit:monit /etc/monit/conf.d /var/monit /var/log/monit /home/monit
