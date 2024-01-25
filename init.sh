fb9e9762d700f675373c988a1ff3362ba00f1080
Lytzeer <lukasportier@gmail.com>
2024-01-25 10:43:07 +0100

    add init bash script

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+    then echo "Veuillez lancer init en tant que root"
+    exit
+fi
+useradd monit -m -s /bin/sh -u 2000
+groupadd monit
+usermod -aG monit monit
+pip install -r ../requirement.txt
+mkdir -p /var/log/monit
+mkdir -p /etc/monit/conf.d
+mkdir -p /var/monit
+chown -R monit:monit /var/log/monit
+chown -R monit:monit /etc/monit/conf.d
+chown -R monit:monit /var/monit
+cp ../conf/conf.json /etc/monit/conf.d/
+cp ../conf/api_conf.json /etc/monit/conf.d/
