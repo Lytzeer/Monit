4c970f5a867981af150b8abbeb5224e4d2465987
Lytzeer <lukasportier@gmail.com>
2024-01-25 10:48:26 +0100

    fix bash script

--- init.sh
+++ init.sh
@@ -7,12 +7,12 @@ fi
 useradd monit -m -s /bin/sh -u 2000
 groupadd monit
 usermod -aG monit monit
-pip install -r /requirement.txt
+pip install -r ./requirement.txt
 mkdir -p /var/log/monit
 mkdir -p /etc/monit/conf.d
 mkdir -p /var/monit
 chown -R monit:monit /var/log/monit
 chown -R monit:monit /etc/monit/conf.d
 chown -R monit:monit /var/monit
-cp /conf/conf.json /etc/monit/conf.d/
-cp /conf/api_conf.json /etc/monit/conf.d/
+cp ./conf/conf.json /etc/monit/conf.d/
+cp ./conf/api_conf.json /etc/monit/conf.d/
