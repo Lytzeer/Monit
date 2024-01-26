68b9878cf6dbbf5d9c3bc9291a686f57b6e97015
Lytzeer <lukasportier@gmail.com>
2024-01-26 08:48:36 +0100

    add chown for service & timer

--- init.sh
+++ init.sh
@@ -14,7 +14,7 @@ mkdir -p /etc/monit/conf.d /var/monit /var/log/monit
 cp ./conf/conf.json ./conf/api_conf.json /etc/monit/conf.d/
 cp ./app/api.py ./app/monit.py /var/monit/
 cp ./monit.service ./monit.timer /etc/systemd/system/
-chown -R monit:monit /etc/monit/conf.d /var/monit /var/log/monit
+chown -R monit:monit /etc/monit/conf.d /var/monit /var/log/monit /etc/systemd/system/monit.*
 systemctl daemon-reload
 systemctl enable monit.timer
 systemctl start monit.timer
